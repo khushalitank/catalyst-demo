9b2122020909d29930829b96ad2c4233289e8239
khushali tank <khushali.tank@1center.co>
2025-03-26 14:48:47 +0530

    catalyst folder

---
+++ core/components/custom/demo.tsx
@@ -0,0 +1,12 @@
+export default function Demo() {
+    return (
+        <div>
+            <p className="bg-blue-400 text-white px-4 py-2 rounded mt-2">
+                This is the Demo component
+            </p>
+            <p className="bg-green-400 text-white px-4 py-2 rounded mt-2">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+        </div>
+    );
+}
