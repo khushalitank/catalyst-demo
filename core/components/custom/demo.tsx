d60f20f6d39cd0d6c102b807dbf3feee2eb237b2
khushali tank <khushali.tank@1center.co>
2025-03-27 14:41:44 +0530

    css change

--- core/components/custom/demo.tsx
+++ core/components/custom/demo.tsx
@@ -7,6 +7,9 @@ export default function Demo() {
             <p className="bg-green-400 text-white px-4 py-2 rounded mt-2">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
+            <p className="bg-green-400 text-white px-4 py-2 rounded mt-2">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
         </div>
     );
 }
